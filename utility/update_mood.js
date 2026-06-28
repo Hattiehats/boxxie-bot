9e7045240372f61c612de6bbc9b4324cfcbcfb91
Harriet Hats <hattiehatshats@gmail.com>
2026-06-29 07:13:58 +1000

    fix: mood updates werent catching correctly

--- utility/update_mood.js
+++ utility/update_mood.js
@@ -3,14 +3,14 @@ import { pickOne, randomActivities } from "./utils.js";
 
 const CHECK_INTERVAL = 6 * 60 * 60 * 1000; // six hours
 
-function moodCycle(client) {
+async function moodCycle(client) {
 	const mood = pickOne(randomActivities);
 	client.user.setActivity(mood.trim(), { type: ActivityType.Custom });
 	console.log(`Bot status set: ${mood.trim()}`);
 }
 
 export function startCycleMood(client) {
-	moodCycle(client);
+	moodCycle(client).catch(e => console.error("err in moodcycle on boot", e));
 	setInterval(() => {
 		moodCycle(client).catch(e => console.error('error in moodCycle:', e));
 	}, CHECK_INTERVAL);
