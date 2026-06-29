e3c8a0dc233d489baf308770478af4a1401e691f
Harriet Hats <hattiehatshats@gmail.com>
2026-06-29 17:51:29 +1000

    fix: typo in reprint

--- commands/reprint.js
+++ commands/reprint.js
@@ -72,7 +72,7 @@ async function reprintMessage(interaction) {
 	let error = false;
 	const ocName = interaction.options.getString("oc");
 
-	const reprintContent = `### ${ocName} has been reconneded without error. Congratulations!`;
+	const reprintContent = `### ${ocName} has been cleanly reconstructed. Please resume your duties.`;
 	const errorContent = `\`\`\`While reconstructing ${ocName}, something went wrong! They've had a RECON ERROR. You may decide the error for yourself, or you may roll 1d10 to pick an error from this table. Effects may be flavored however you like.\`\`\`\n**You come back from the recon process...**\n> \`1.)\` - With a different hair and/or eye color.\n> \`2.)\` - 1d6 inches shorter.\n> \`3.)\` - 1d6 inches taller.\n> \`4.)\` - Differently colored blood.\n> \`5.)\` - With impaired functioning in part of their body.\n> \`6.)\` - With a seemingly permanent illness they didn't have before. \n> \`7.)\` - With sudden chronic pain.\n> \`8.)\` - With personality change. (Less irritable, etc.)\n> \`9.)\` - With a gap in their memory.\n> \`10.)\` - Missing part of their body.\`\`\`This error will impact you until your next recon.\`\`\``;
 	const reprintConfirmMessage = [
 		new ContainerBuilder()
